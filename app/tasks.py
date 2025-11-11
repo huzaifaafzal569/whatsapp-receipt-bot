570f520eb2d59eb11d4208d4e517f1612c3c8f19
Huzaifa <huzaifaafzal569@gmail.com>
2025-11-11 22:49:53 +0500

    deive

--- app/tasks.py
+++ app/tasks.py
@@ -372,7 +372,7 @@ def process_receipt(image_base64: str, metadata: Dict[str, Any]) -> Dict[str, An
     r'(?:\s*(?:or|o)?\s*CTRL)?'
     r'\s*(?:de\s+)?(?:Mercado\s*Pago)?\s*[:\-]?\s*([A-Za-z0-9\s\n]+)',
 
-    'numeric_op': r'(?:n°?\s+de\s+operaci[oó]n|n[uú]mero\s+de\s+operaci[oó]n\s+de\s+Mercado\s*Pago|nro\.|n°?\s*control)\s*[:\-]?\s*([0-9]+)',
+    'numeric_op': r'(?:n°?\s+de\s+operaci[oó]n|n[uú]mero\s+de\s+operaci[oó]n\s+de\s+Mercado\s*Pago|nro\.|n°?\s*control|referencia)\s*[:\-]?\s*([0-9]+)',
     # 'numeric_op': r'(?:n[uú]mero\s+de\s+operaci[oó]n\s+de\s+Mercado\s*Pago|referen[cñ]ia|control|transacci[oó]n|identificaci[oó]n|operation)\s*[:\-]?\s*([0-9]+?)',
     # 'alphanumeric_op': r'(?:c[oó]digo\s+de\s+identificaci[oó]n|referencia|control|comprobante|transacci[oó]n|operation)\s*[:\-]?\s*([A-Za-z0-9\s\n]+)',
     'alphanumeric_op': r'(?:C[oó]digo\s+de\s+transacci[oó]n|C[oó]digo\s+de\s+identificaci[oó]n|referencia|control|transacci[oó]n|operation|C[oó]mprobante)\s*[:\-]?\s*([A-Za-z0-9\s\n\-]+?)' # Note the non-greedy '?'
@@ -532,7 +532,7 @@ def process_receipt(image_base64: str, metadata: Dict[str, Any]) -> Dict[str, An
 
 # --- Upload image to Drive and get link ---
     try:
-        image_link = upload_file_and_get_link(local_path=image_path, dest_name=os.path.basename(image_path), supplier_folder=folder_name)
+        # image_link = upload_file_and_get_link(local_path=image_path, dest_name=os.path.basename(image_path), supplier_folder=folder_name)
         extracted_data['image_URL'] = image_link
     except Exception as e:
         logger.warning(f"Drive upload failed: {e}")
